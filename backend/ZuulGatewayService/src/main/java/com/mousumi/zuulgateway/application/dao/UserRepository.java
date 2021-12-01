2d979dd073e06fe684205a4bf52e75f0b52fb407
MousumiDas12 <86611152+MousumiDas12@users.noreply.github.com>
2021-12-01 17:25:34 +0530

    Add files via upload

---
+++ backend/ZuulGatewayService/src/main/java/com/mousumi/zuulgateway/application/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.mousumi.zuulgateway.application.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mousumi.zuulgateway.application.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>
+{
+	public User findByUsername(String username);
+	public User findByUsernameOrEmail(String username, String email);
+	public User findByConfirmationToken(String token);
+}
---
+++ backend/ZuulGatewayService/src/main/java/com/mousumi/zuulgateway/application/service/MailService.java
@@ -0,0 +1,7 @@
+package com.mousumi.zuulgateway.application.service;
+
+import com.mousumi.zuulgateway.application.model.Mail;
+
+public interface MailService {
+	public void sendEmail(Mail mail);
+}
