4910c861ef9dcf10ff0fae0b1603999cc03c91fd
MousumiDas12 <86611152+MousumiDas12@users.noreply.github.com>
2021-12-01 17:24:26 +0530

    Add files via upload

---
+++ backend/StockPriceApplication/src/main/java/com/mousumi/stockprice/application/feignclient/SectorClient.java
@@ -0,0 +1,16 @@
+package com.mousumi.stockprice.application.feignclient;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.mousumi.stockprice.application.dto.CompanyDto;
+
+@FeignClient("sector-service")
+public interface SectorClient 
+{
+	@GetMapping("/sectors/{sectorName}/companies")
+	public List<CompanyDto> getSectorCompanies(@PathVariable String sectorName);
+}
