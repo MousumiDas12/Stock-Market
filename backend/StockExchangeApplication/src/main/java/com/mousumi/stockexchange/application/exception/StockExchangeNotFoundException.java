29f710e12c0c430e0c444ac6699e877da0764099
MousumiDas12 <86611152+MousumiDas12@users.noreply.github.com>
2021-12-01 17:23:22 +0530

    Add files via upload

---
+++ backend/StockExchangeApplication/src/main/java/com/mousumi/stockexchange/application/exception/StockExchangeNotFoundException.java
@@ -0,0 +1,13 @@
+package com.mousumi.stockexchange.application.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockExchangeNotFoundException extends Throwable
+{
+	private String message;
+}
